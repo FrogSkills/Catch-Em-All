65e3085f1a76f3af57398b7db6e3458e18d85ed9
Miguel <mgamez20@icloud.com>
2025-12-31 19:44:35 -0500

    Initial Commit

---
+++ Catch Em All/Catch_Em_AllApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catch_Em_AllApp.swift
+//  Catch Em All
+//
+//  Created by Miguel on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Catch_Em_AllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
